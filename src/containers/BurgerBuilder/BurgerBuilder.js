b0a0b2a5ce7c431e692af47717895d9bc00a34ed
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-11-19 23:06:50 +1100

    ReactRouterStage1

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,29 +85,21 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Terry Y Wang",
-        address: {
-          street: "88 George Street",
-          postCode: "2000",
-          country: "Australia"
-        },
-        email: "softwaredeveloper@mvpstudio.com"
-      },
-      deliveryMethod: "fastest courier"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString
+    });
   };
 
   render() {
---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import Button from "../../../components/UI/Button/Button";
+import Spinner from "../../../components/UI/Spinner/Spinner";
+import classes from "../ContactData/ContactData.module.css";
+import axios from "../../../axios-orders";
+
+class ContactData extends Component {
+  state = {
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      postCode: ""
+    },
+    loading: false
+  };
+
+  orderHandler = event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: "Terry Y Wang",
+        address: {
+          street: "88 George Street",
+          postCode: "2000",
+          country: "Australia"
+        },
+        email: "softwaredeveloper@mvpstudio.com"
+      },
+      deliveryMethod: "fastest courier"
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+  };
+
+  render() {
+    let form = (
+      <form>
+        <input
+          className={classes.Input}
+          type="text"
+          name="name"
+          placeholder="Your Name"
+        />
+        <input
+          className={classes.Input}
+          type="email"
+          name="email"
+          placeholder="Your Email"
+        />
+        <input
+          className={classes.Input}
+          type="text"
+          name="street"
+          placeholder="Street"
+        />
+        <input
+          className={classes.Input}
+          type="text"
+          name="postCode"
+          placeholder="Post Code"
+        />
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your Contact Details</h4>
+        {form}
+      </div>
+    );
+  }
+}
+
+export default ContactData;
