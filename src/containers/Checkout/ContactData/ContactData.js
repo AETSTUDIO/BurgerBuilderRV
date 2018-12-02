07e8da71788210c263a4cc59f36c8bb3111646af
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-12-02 21:18:32 +1100

    Implement Basic Redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "../ContactData/ContactData.module.css";
 import axios from "../../../axios-orders";
 import Input from "../../../components/UI/Input/Input";
+import  {connect } from "react-redux";
 
 class ContactData extends Component {
   state = {
@@ -102,8 +103,8 @@ class ContactData extends Component {
     }
     console.log(formData);
     const order = {
-      ingredients: this.props.ingredients,
-      price: this.props.price,
+      ingredients: this.props.ings,
+      price: this.props.totalPrice,
       orderData: formData
     };
     axios
@@ -192,4 +193,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    totalPrice: state.totalPrice
+  }
+}
+
+export default connect(mapStateToProps)(ContactData);
