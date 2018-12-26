676a530989f6031fc598c3370423d9d4651a63c0
Terry (Yan) Wang <enjoybusy@hotmail.com>
2018-12-27 00:09:53 +1100

    Add Some testing files

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import withErrorHandler from "../withErrorHandler/withErrorHandler";
 import axios from "../../axios-orders";
 import * as actions from "../../store/actions/index";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false
   };
